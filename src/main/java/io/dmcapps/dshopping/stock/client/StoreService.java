c6e8cb4c12c6aa7126587b65c520239c7f5bc5e5
danielmachado86 <danielmcis@gmail.com>
2020-08-15 13:00:00 -0500

    changes in store client

--- src/main/java/io/dmcapps/dshopping/stock/client/StoreService.java
+++ src/main/java/io/dmcapps/dshopping/stock/client/StoreService.java
@@ -2,6 +2,8 @@ package io.dmcapps.dshopping.stock.client;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,9 +14,10 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @RegisterRestClient
 public interface StoreService {
-
+    
     @GET
-    Store getNearByStores(
+    @Path("/search")
+    List<Store> getNearByStores(
         @QueryParam("lon") double lon,
         @QueryParam("lat") double lat,
         @QueryParam("range") int range);
