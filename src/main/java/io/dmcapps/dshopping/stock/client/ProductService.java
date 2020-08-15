23dce7fd1c49060edda0f2598cd298a3289a705f
danielmachado86 <danielmcis@gmail.com>
2020-08-15 12:59:29 -0500

    Changes in product client

--- src/main/java/io/dmcapps/dshopping/stock/client/ProductService.java
+++ src/main/java/io/dmcapps/dshopping/stock/client/ProductService.java
@@ -2,9 +2,12 @@ package io.dmcapps.dshopping.stock.client;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/api/products")
@@ -14,5 +17,5 @@ public interface ProductService {
 
     @GET
     @Path("/search")
-    Product searchProduct();
+    List<Product> searchProduct(@QueryParam("q") String  search);
 }
